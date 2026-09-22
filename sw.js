f9436a1300fdb39d04e755b0243b33410bb156cb
Riccig98 <163731859+Riccig98@users.noreply.github.com>
2026-09-23 00:39:49 +0200

    Replace service worker with minimal PWA shell

--- sw.js
+++ sw.js
@@ -1,23 +1,27 @@
-const CACHE="chord-ear-trainer-v20";
-const FILES=[
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./db.js",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+const CACHE="chord-ear-trainer-v21";
+const SHELL=[
+  "/music/",
+  "/music/index.html",
+  "/music/styles.css",
+  "/music/app.js",
+  "/music/db.js",
+  "/music/manifest.json",
+  "/music/icons/icon-192.png",
+  "/music/icons/icon-512.png"
 ];
 
 self.addEventListener("install",event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));
+  event.waitUntil(
+    caches.open(CACHE).then(cache=>cache.addAll(SHELL))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate",event=>{
   event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    caches.keys().then(keys=>
+      Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))
+    )
   );
   self.clients.claim();
 });
@@ -25,19 +29,22 @@ self.addEventListener("activate",event=>{
 self.addEventListener("fetch",event=>{
   if(event.request.method!=="GET")return;
 
-  const requestUrl=new URL(event.request.url);
-  const isAppAsset=requestUrl.origin===self.location.origin&&requestUrl.pathname.startsWith("/music/");
-
-  if(isAppAsset){
+  if(event.request.mode==="navigate"){
     event.respondWith(
-      fetch(event.request).then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-        return response;
-      }).catch(()=>caches.match(event.request).then(r=>r||caches.match("./index.html")))
+      fetch(event.request).catch(()=>caches.match("/music/index.html"))
     );
     return;
   }
 
-  event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(cached=>
+      cached||fetch(event.request).then(response=>{
+        if(response&&response.ok&&new URL(event.request.url).origin===self.location.origin){
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+        }
+        return response;
+      })
+    )
+  );
 });
