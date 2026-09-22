75bad524772f79c1419965b18015f47c66f6913f
Riccig98 <163731859+Riccig98@users.noreply.github.com>
2026-09-23 01:49:55 +0200

    Add minimal diagnostic service worker

---
+++ install-test/sw.js
@@ -0,0 +1,22 @@
+const CACHE="chord-install-test-v1";
+self.addEventListener("install",event=>{
+  event.waitUntil(
+    caches.open(CACHE).then(cache=>cache.addAll([
+      "./",
+      "./index.html",
+      "./manifest.webmanifest",
+      "../icons/icon-192.png",
+      "../icons/icon-512.png"
+    ]))
+  );
+  self.skipWaiting();
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  event.respondWith(
+    fetch(event.request).catch(()=>caches.match(event.request))
+  );
+});
