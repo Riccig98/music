7ed3519bed8603ebc6fce8495964eb841a1545f1
Riccig98 <163731859+Riccig98@users.noreply.github.com>
2026-09-23 03:52:23 +0200

    Cache 700px test icon

--- install-test/sw.js
+++ install-test/sw.js
@@ -1,22 +1,42 @@
-const CACHE="chord-install-test-v1";
-self.addEventListener("install",event=>{
-  event.waitUntil(
-    caches.open(CACHE).then(cache=>cache.addAll([
-      "./",
-      "./index.html",
-      "./manifest.webmanifest",
-      "../icons/icon-192.png",
-      "../icons/icon-512.png"
-    ]))
-  );
-  self.skipWaiting();
-});
-self.addEventListener("activate",event=>{
-  event.waitUntil(self.clients.claim());
-});
-self.addEventListener("fetch",event=>{
-  if(event.request.method!=="GET")return;
+var GHPATH = "/music/install-test";
+var APP_PREFIX = "chordtest_";
+var VERSION = "version_002";
+var URLS = [
+  GHPATH + "/",
+  GHPATH + "/index.html",
+  "/music/icons/icon-700.png"
+];
+
+var CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener("fetch",function(event){
   event.respondWith(
-    fetch(event.request).catch(()=>caches.match(event.request))
+    caches.match(event.request).then(function(request){
+      return request || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("install",function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(URLS);
+    })
+  );
+});
+
+self.addEventListener("activate",function(event){
+  event.waitUntil(
+    caches.keys().then(function(keyList){
+      var cacheWhitelist=keyList.filter(function(key){
+        return key.indexOf(APP_PREFIX)===0;
+      });
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map(function(key){
+        if(cacheWhitelist.indexOf(key)===-1){
+          return caches.delete(key);
+        }
+      }));
+    })
   );
 });
